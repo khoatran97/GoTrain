bcd2af3763bed6475e0275def2cf891748465ed0
Nguyễn Ngọc Xuân Mỹ <nnxmy97@gmail.com>
2018-05-10 22:18:06 +0700

    [Update] main.handlebars
    - thêm {{{_sections.li}}}
    [Update] home.hbs
    [Add] service.js
    - express-handlebars-sections

--- service.js
+++ service.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var hbs = require('express-handlebars');
+var express_handlebars_sections = require('express-handlebars-sections');
 var path = require('path');
 
 var app = express()
 
-app.engine('handlebars', hbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs({
+	defaultLayout: 'main',
+	helpers: {
+        section: express_handlebars_sections()
+    }
+}));
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
